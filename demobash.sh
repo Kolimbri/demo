6bc983612e6e407ca0ea51dd660cb8262568174c
u237525 <jerome.egger@sbb.ch>
2023-01-11 16:06:58 +0100

    Ich habe eine Zeile hinzugefügt

--- demobash.sh
+++ demobash.sh
@@ -7,3 +7,6 @@ echo "Give me the diameter of the cylinder in cm"
 read diameter
 echo "The volume of this cylinder is"
 echo "( $diameter/2 )^2 * $height * 3.14159" | bc
+
+#it's magic
+echo Das war schon alles
